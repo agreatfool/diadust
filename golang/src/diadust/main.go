e7d474fac7089e5bf485ae39af4fb71003a39a4a
Jonathan Dai <nightghost5078@gmail.com>
2018-05-31 14:54:14 +0800

    Add golang grpc server sample

--- golang/src/diadust/main.go
+++ golang/src/diadust/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"google.golang.org/grpc"
+	pb "./rpc"
+)
 
+type server struct{}
+
+func (s *server) FetchImages(rect *pb.Filter, stream pb.ImageService_FetchImagesServer) error {
+	images := [...]*pb.Image{
+		{Id:1, Uuid:"uuid1", OriginalName:"originalname1", Path:"path1", Tag:[]int32{1,2,3}, Created:123},
+		{Id:2, Uuid:"uuid2", OriginalName:"originalname2", Path:"path2", Tag:[]int32{1,2,3}, Created:123},
+		{Id:3, Uuid:"uuid3", OriginalName:"originalname3", Path:"path3", Tag:[]int32{1,2,3}, Created:123},
+		{Id:4, Uuid:"uuid4", OriginalName:"originalname4", Path:"path4", Tag:[]int32{1,2,3}, Created:123},
+		{Id:5, Uuid:"uuid5", OriginalName:"originalname5", Path:"path5", Tag:[]int32{1,2,3}, Created:123},
+	}
+
+	for _, img := range images {
+		if err := stream.Send(img); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterImageServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
