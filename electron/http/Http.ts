f3019dd5646cb34ebd24b2b95aa5ddb87eda620f
Jonathan Dai <nightghost5078@gmail.com>
2018-06-01 18:09:49 +0800

    Enhance http request lb

--- electron/http/Http.ts
+++ electron/http/Http.ts
@@ -1,6 +1,8 @@
 import * as LibUrl from 'url';
 import fetch from 'node-fetch';
 
+const pkg = require('../../package.json');
+
 export async function post(uri, data: {[key: string]: any}) {
     return new Promise((resolve, reject) => {
         fetch(LibUrl.resolve('http://127.0.0.1:13000', uri), {
@@ -11,6 +13,7 @@ export async function post(uri, data: {[key: string]: any}) {
                 'Content-Type': 'application/json',
                 'Content-Encoding': 'deflate,gzip',
                 'Accept-Encoding': 'deflate,gzip',
+                'User-Agent': `diadust-${pkg.version}`,
             }
         }).then(_ => _.json()).then(_ => resolve(_)).catch(err => {
             if (!err) {
