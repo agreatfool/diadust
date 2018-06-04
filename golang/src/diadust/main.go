bfa125b6e2e45605108c7366a758a0cb4894e293
Jonathan Dai <nightghost5078@gmail.com>
2018-06-04 15:10:50 +0800

    Write golang beego config into binary command instead of a config file

--- golang/src/diadust/main.go
+++ golang/src/diadust/main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"diadust/controllers"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+	beego.BConfig.Listen.HTTPAddr = "127.0.0.1"
+	beego.BConfig.Listen.HTTPPort = 13000
+	beego.BConfig.CopyRequestBody = true
+	beego.BConfig.RunMode = "dev"
+	beego.BConfig.EnableGzip = true
+	context.InitGzip(
+		beego.AppConfig.DefaultInt("gzipMinLength", -1),
+		beego.AppConfig.DefaultInt("gzipCompressLevel", 7),
+		beego.AppConfig.DefaultStrings("includedMethods", []string{"GET", "POST"}),
+	)
+	beego.BConfig.WebConfig.AutoRender = false
+
 	beego.Router("/image/fetch", &controllers.ImageController{}, "post:Fetch")
+
 	beego.Run()
 }
